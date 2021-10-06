7998c7b1517d34506881ad2604826800074494e0
Pratik Goyal <pratik.goyal@mendix.com>
2021-10-06 14:08:14 +0000

    added module of pieDoughnutChart

--- theme/native/main.js
+++ theme/native/main.js
@@ -1,1 +1,44 @@
-export {};
+const myCustomSyling = {
+    slices:
+    {
+        innerRadius: 50,
+        customStyles: {
+            mySlice1: {
+                slice: {
+                    color: 'gray'
+            },
+            label: {
+                  fontStyle: "italic"
+                }
+        },
+            mySlice2: {
+                slice: {
+                    color: 'brown'
+                }
+            },
+            mySlice3: {
+                slice: {
+                    color: 'pink'
+                },
+                label: {
+                    fontStyle: "italic"
+                    }
+            },
+            mySlice4: {
+                slice: {
+                    color: 'aqua'
+                }
+            },
+            mySlice5: {
+                slice: {
+                    color: 'yellow'
+                },
+                label: {
+                    fontStyle: "italic"
+                    }
+            }
+        }
+    }
+}
+
+export { myCustomSyling };
