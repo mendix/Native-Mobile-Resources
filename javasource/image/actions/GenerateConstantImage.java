da13bf6ba97b4f9afa96e4539b6662da774f889b
Uraz Akgultan <uraz@akgultan.com>
2023-08-16 05:10:35 +0000

    Update SP to 9.24.1

--- javasource/image/actions/GenerateConstantImage.java
+++ javasource/image/actions/GenerateConstantImage.java
@@ -50,6 +50,7 @@ public class GenerateConstantImage extends CustomJavaAction<java.lang.Void>
 
 	/**
 	 * Returns a string representation of this action
+	 * @return a string representation of this action
 	 */
 	@java.lang.Override
 	public java.lang.String toString()
