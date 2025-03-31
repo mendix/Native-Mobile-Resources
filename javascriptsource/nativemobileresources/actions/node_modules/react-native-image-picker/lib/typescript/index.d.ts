d71c37d3db793b558cf2d11c064d38a57b8dfff6
karahanharunn <karahan.harunn@gmail.com>
2025-03-31 09:59:04 +0200

    update to version 10.21

--- javascriptsource/nativemobileresources/actions/node_modules/react-native-image-picker/lib/typescript/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { CameraOptions, ImageLibraryOptions, Callback } from './types';
-export * from './types';
-export declare function launchCamera(options: CameraOptions, callback?: Callback): Promise<import("./types").ImagePickerResponse>;
-export declare function launchImageLibrary(options: ImageLibraryOptions, callback?: Callback): Promise<import("./types").ImagePickerResponse>;
