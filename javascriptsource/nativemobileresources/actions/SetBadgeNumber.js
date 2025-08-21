94abfb84f5f9bda7b64f7f9c96abb80f3537169c
MOO <moo@mendix.com>
2025-08-21 13:13:40 +0000

    Updated JS actions and widgets

--- javascriptsource/nativemobileresources/actions/SetBadgeNumber.js
+++ javascriptsource/nativemobileresources/actions/SetBadgeNumber.js
@@ -1,5 +1,5 @@
-import { Platform, NativeModules } from 'react-native';
-import PushNotification from 'react-native-push-notification';
+import { NativeModules } from 'react-native';
+import notifee from '@notifee/react-native';
 
 // BEGIN EXTRA CODE
 // END EXTRA CODE
@@ -9,10 +9,9 @@ import PushNotification from 'react-native-push-notification';
  */
 async function SetBadgeNumber(badgeNumber) {
     // BEGIN USER CODE
-    // Documentation https://github.com/zo0r/react-native-push-notification
-    const isIOS = Platform.OS === "ios";
-    if (NativeModules && isIOS && !NativeModules.RNCPushNotificationIOS) {
-        return Promise.reject(new Error("Notifications module is not available in your app"));
+    // Documentation Documentation https://github.com/invertase/notifee
+    if (NativeModules && !NativeModules.NotifeeApiModule) {
+        return Promise.reject(new Error("Notifee native module is not available in your app"));
     }
     if (!badgeNumber) {
         return Promise.reject(new Error("Input parameter 'Badge number' is required"));
@@ -20,7 +19,7 @@ async function SetBadgeNumber(badgeNumber) {
     if (badgeNumber.lt(0)) {
         return Promise.reject(new Error("Input parameter 'Badge number' should be zero or greater"));
     }
-    return PushNotification.setApplicationIconBadgeNumber(Number(badgeNumber));
+    return notifee.setBadgeCount(Number(badgeNumber));
     // END USER CODE
 }
 
