3641d6ecd5dc3a61788746ba5782b4776125189b
MOO <moo@mendix.com>
2026-09-02 08:56:23 +0000

    Updated JS actions

--- javascriptsource/nanoflowcommons/actions/RequestLocationPermission.js
+++ javascriptsource/nanoflowcommons/actions/RequestLocationPermission.js
@@ -66,7 +66,7 @@ async function RequestLocationPermission() {
         }
         if (Platform.OS === "android") {
             const hasPermission = await hasPermissionAndroid();
-            return hasPermission !== null && hasPermission !== void 0 ? hasPermission : false;
+            return hasPermission !== null && hasPermission !== undefined ? hasPermission : false;
         }
         return Promise.reject(new Error("Unsupported platform"));
     };
